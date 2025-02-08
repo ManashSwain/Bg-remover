ff8dc27586bcb7c8cf23a524e78d8616e3f7445e
ManashSwain <manash0802@gmail.com>
2025-02-08 16:07:03 +0530

    added router logic

---
+++ server/Routes/user.route.js
@@ -0,0 +1,6 @@
+import express from "express" ;
+import { clerkWebhook } from "../Controllers/user.controller.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/webhooks" , clerkWebhook);
