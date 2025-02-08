87965908262be6f319ec7414a50967bf20cadd6e
ManashSwain <manash0802@gmail.com>
2025-02-08 12:23:14 +0530

    implemented backeend structure

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// cors options 
+
+const corsOptions = {
+    origin: ["http://localhost:5173", "https://yourfrontend.com"], 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, 
+  };
+
+// middlewares 
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Backend route 
+app.get("/" , (req,res)=>{
+  res.status(200).send("Backend API");
+});
+
+app.listen(PORT , ()=>{
+  console.log(`App is listening on Port ${PORT}`);
+})
