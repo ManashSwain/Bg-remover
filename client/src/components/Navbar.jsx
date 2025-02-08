227c980c709553bb1ee07f50983a3423902ff233
ManashSwain <manash0802@gmail.com>
2025-02-08 11:47:15 +0530

    added clerk authentication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,14 @@
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+
+    const {openSignIn} = useClerk();
+    const { isSignedIn, isLoaded } = useUser();
+    
+  if (!isLoaded) {
+    return <div>Loading...</div>
+  }
   return (
     <>
     <header className="text-gray-600 body-font px-28">
@@ -13,12 +21,16 @@ const Navbar = () => {
       </svg>
       <span className="ml-3 text-xl">Tailblocks</span>
     </a>
+    {isSignedIn ? <UserButton /> : 
+    <button onClick={()=>{openSignIn({})}} className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Get Started
+    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+      <path d="M5 12h14M12 5l7 7-7 7"></path>
+    </svg>
+  </button> }
     
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Get Started
-      <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
+    
+
+ 
   </div>
 </header>
 
