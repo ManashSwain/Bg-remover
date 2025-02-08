b656812ce6d38c1176188f6633980a268432ced9
ManashSwain <manash0802@gmail.com>
2025-02-08 15:00:15 +0530

    configured vercel.json

---
+++ server/Controllers/user.controller.js
@@ -0,0 +1,11 @@
+// controller function to manage clerk user with mongodb database 
+
+const clerkWebhook = async ( req, res)=>{
+  try {
+
+  }
+  catch(err){
+  res.status(200).json({success : false , message : "Some error occured"});
+  }
+}
+
