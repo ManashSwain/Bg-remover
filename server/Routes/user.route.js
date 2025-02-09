68f09d89728efac1e3b6160920b0870008f7aac3
ManashSwain <manash0802@gmail.com>
2025-02-09 11:03:21 +0530

    configured user router for displaying credits

--- server/Routes/user.route.js
+++ server/Routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express" ;
-import { clerkWebhook } from "../Controllers/user.controller.js";
+import { clerkWebhook, userCredits } from "../Controllers/user.controller.js";
+import { authUser } from "../Middleware/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks" , clerkWebhook);
+userRouter.get("credits" , authUser , userCredits);
 
 
 export default userRouter;
